59c0951c97b43b3127168577efff87c9126b225d
Axel Pahl <axelpahl@gmx.de>
2015-08-20 18:15:07 +0200

    - added a few function stubs to fill in the next days....

--- pandas_tools.py
+++ pandas_tools.py
@@ -8,7 +8,10 @@ Created on Wed Jul 29 12:20:19 2015
 
 import pandas as pd
 
+from rdkit.Chem import AllChem as Chem
+
 from . import tools, hc_tools as hct
+
 
 def df_from_sdf_list(sdf_list, id_prop=None, props=None, set_index=True):
     """Generate a Pandas dataframe from the properties of a list of molecules.
@@ -29,3 +32,52 @@ def df_from_sdf_list(sdf_list, id_prop=None, props=None, set_index=True):
         df = df.set_index(guessed_id)
     
     return df
+
+
+def join_data_from_file(df, fn):
+    """
+    join data from file (e.g. Smiles, biol. data (tab-sep.) ) by index (set to Compound_Id) to df
+    index in df has to be set to Compound_Id
+    """
+    data_df = pd.concat([df, pd.read_table(fn, index_col=df.index.name)], axis=1, join_axes=[df.index])
+    
+    return data_df
+
+
+def df_to_sdf_list(df):
+    """
+    returns: list of mols
+    """
+    pass
+
+
+def df_show_table(df):
+    """
+    show df as mol_table
+    """
+    pass
+
+
+def move_col_left(df, col):
+    """
+    # this moves the last col to the left:
+    cols = list(mol_df.columns.values)
+    new_cols = [cols[-1]]
+    new_cols.extend(cols[:-1])
+    mol_df = mol_df[new_cols]
+    
+    return df
+    """
+    pass
+
+
+def align_molecules(df, qry, mol_col="molecule"):
+    """
+    Align all molecules in a df to a given query molecule,
+    e.g. after a substructure query.
+    operates inline, returns nothing"""
+    
+    Chem.Compute2DCoords(qry)
+    for mol in df[mol_col]:
+        Chem.GenerateDepictionMatching2DStructure(mol, qry)
+    
