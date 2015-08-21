26c8f8c32c9f0192ba9e82132be7aee1c7650ea5
Axel Pahl <axelpahl@gmx.de>
2015-08-21 10:31:29 +0200

    - improved join_data_from_file() - implemented move_col()

--- pandas_tools.py
+++ pandas_tools.py
@@ -9,6 +9,7 @@ Created on Wed Jul 29 12:20:19 2015
 import pandas as pd
 
 from rdkit.Chem import AllChem as Chem
+from rdkit.Chem import PandasTools as PT
 
 from . import tools, hc_tools as hct
 
@@ -34,12 +35,62 @@ def df_from_sdf_list(sdf_list, id_prop=None, props=None, set_index=True):
     return df
 
 
-def join_data_from_file(df, fn):
+def move_col(df, col, new_pos=1):
     """
-    join data from file (e.g. Smiles, biol. data (tab-sep.) ) by index (set to Compound_Id) to df
-    index in df has to be set to Compound_Id
+    Put column col on position new_pos.
+    """
+    
+    cols = list(df.columns.values)
+    index = cols.index(col)  # raises ValueError if not found
+    cols_len = len(cols)
+    if new_pos > cols_len - 1:
+        new_pos = cols_len - 1
+    
+    if index == new_pos:
+        print("{} is already on position {}".format(col, new_pos))
+        return df
+    
+    new_cols = []
+    for i, c in enumerate(cols):
+        if i == index: continue
+        if i == new_pos:
+            new_cols.append(col)
+        new_cols.append(c)
+        
+    new_df = df[new_cols]
+    
+    return new_df
+    
+
+
+def join_data_from_file(df, fn, dropna=True, gen_struct=True, remove_smiles=True):
+    """
+    Join data from file (e.g. Smiles, biol. data (tab-sep.) ) by index (set to Compound_Id) to df.
+    If gen_struct is True and Smiles could be found in the resulting df,
+      then the structure is generated and moved to first column.
+    If remove_smiles is True, the Smiles column will be dropped.
+    index in df has to be set to the Compound_Id.
+    returns new df
     """
     data_df = pd.concat([df, pd.read_table(fn, index_col=df.index.name)], axis=1, join_axes=[df.index])
+    
+    if dropna:
+        data_df = data_df.dropna(axis=1)
+    
+    if gen_struct:
+        smiles_col = None
+        for col in list(data_df.columns.values):
+            if "smiles" in col.lower():
+                smiles_col = col
+                break
+        
+        if smiles_col:
+            PT.AddMoleculeColumnToFrame(data_df, smilesCol=smiles_col, molCol='mol', includeFingerprints=False)
+        
+            if remove_smiles:
+                data_df = data_df.drop(smiles_col, axis=1)
+        
+            data_df = move_col(data_df, "mol", new_pos=0)
     
     return data_df
 
@@ -58,20 +109,7 @@ def df_show_table(df):
     pass
 
 
-def move_col_left(df, col):
-    """
-    # this moves the last col to the left:
-    cols = list(mol_df.columns.values)
-    new_cols = [cols[-1]]
-    new_cols.extend(cols[:-1])
-    mol_df = mol_df[new_cols]
-    
-    return df
-    """
-    pass
-
-
-def align_molecules(df, qry, mol_col="molecule"):
+def align_molecules(df, qry, mol_col="mol"):
     """
     Align all molecules in a df to a given query molecule,
     e.g. after a substructure query.
